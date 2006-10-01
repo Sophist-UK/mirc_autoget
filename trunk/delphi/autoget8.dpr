6853b1f47d6bfc2d60a1009b2d9fead9b3ee93f3
dukelupus <dukelupus@43372145-bd1e-0410-9a03-f78cdf703759>
2006-10-01 11:13:39 +0000

    Adding base files

---
+++ trunk/delphi/autoget8.dpr
@@ -0,0 +1,54 @@
+library autoget8;
+
+uses
+  SysUtils,
+  Classes,
+  mIRCManager in 'support\mIRCManager.pas',
+  main in 'units\main.pas' {fMain};
+
+var
+  mIRC: tMircManager;
+
+procedure LoadDll(LoadInfo: PLoadInfo); stdcall; export;
+begin
+  LoadInfo.mKeep := true; // don`t release DLL after use
+  mirc := tmircmanager.Create(Loadinfo.mHwnd);
+end;
+
+function UnloadDll(mTimeOut: integer): integer; stdcall; export;
+begin
+  if mTimeOut = 0 then
+
+  begin
+    result := 1;
+    try
+      if fMain <> nil then freeandnil(fMain);
+      mirc.Free;
+    except
+    end;
+  end
+  else
+  begin
+    result := 0;
+  end;
+end;
+
+
+//show mainform
+function Show(mWnd, aWnd: HWND; data, parms: PChar; show, nopause: boolean):
+  integer; stdcall;
+begin
+  result := 1;
+  if fMain = nil then
+    fMain := tfMain.Create(nil);
+//TODO: Re-implement application-level exception handler
+  //  application.OnException := main.MainForm.AppException;
+  fMain.Show;
+end;
+
+{$R *.res}
+
+begin
+end.
+
+ 
---
+++ trunk/delphi/units/main.pas
@@ -0,0 +1,24 @@
+unit main;
+
+interface
+
+uses
+  Windows,SysUtils,  Classes,  Controls, Forms;
+
+
+type
+  TfMain = class(TForm)
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  fMain: TfMain;
+
+implementation
+
+{$R *.dfm}
+
+end.
